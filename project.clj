fbc14787e69fcc7525acfbf0da3afd239fa095ba
stoutfiles <shawn.stout@knetik.com>
2017-03-08 10:30:33 -0500

    JSAPI Clojure API update

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject knetik-platform-api-documentation-latest
+ "latest
+"
+  :description "This is the spec for the Knetik API.  Use this in conjunction with the documentation found at https://knetikcloud.com"
+  :url "http://www.knetik.com"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [cheshire "5.5.0"]])
